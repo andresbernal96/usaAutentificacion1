ce4a6236fc4bc5df25ad70e145749a0af88571a0
andresbernal96 <andresfbernalm@hotmail.com>
2017-03-24 14:22:31 -0500

    Prueba pw

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ passport.use(
       clientID: '1815710885419830',
       clientSecret: '8579395b47fe5023fad41af7e9206ab0',
       callbackURL: "https://usaregistro.herokuapp.com/users/auth/facebook/callback",
-      profileFields: ["email", "displayName","gender"]
+      profileFields: ["email", "displayName","gender","password"]
     },
     function (token, refreshToke, profile, done) {
       process.nextTick(
@@ -68,6 +68,7 @@ passport.use(
                 newUser.facebook.token = token;
                 newUser.facebook.name = profile.displayName;
                 newUser.facebook.email = profile.gender;
+                newUser.facebook.email = profile.password;
                 newUser.save(
                   function (err) {
                     if (err)
